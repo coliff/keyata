fd83db92a1fd1bdc76e8b128a66912f0f377dc1f
coliff <christianoliff@yahoo.com>
2018-09-05 11:49:58 +0900

    screenshots added and build script

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
+  grunt.initConfig({
+    imagemin: {
+      static: {
+        options: {
+          optimizationLevel: 4
+        }
+      },
+      dynamic: {
+        options: {
+          optimizationLevel: 4,
+          svgoPlugins: [{ removeViewBox: false }]
+        },
+        files: [
+          {
+            expand: true,
+            cwd: "extension/",
+            src: ["**/*.{png}"],
+            dest: "extension/"
+          }
+        ]
+      }
+    },
+    compress: {
+      main: {
+        options: {
+          archive: "extension.zip"
+        },
+        files: [
+          {
+            expand: true,
+            src: "**",
+            cwd: "extension/",
+            dest: "/"
+          }
+        ]
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.registerTask("default", ["imagemin", "compress"]);
+};
