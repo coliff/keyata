15317838308da2a0d77927b3b2fe7032f917aaad
Christian Oliff <1212885+coliff@users.noreply.github.com>
2022-04-17 11:17:02 +0900

    updates to build and config

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,5 @@
 const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
 const zip = require("gulp-zip");
-
-gulp.task("imagemin", () =>
-  gulp
-    .src("extension/icons/**")
-    .pipe(imagemin())
-    .pipe(gulp.dest("extension/icons"))
-);
 
 gulp.task("default", () =>
   gulp
